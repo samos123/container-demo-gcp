326d3ffaa04984e0c965fcd995093a27517e5888
Sam Stoelinga <stoelinga@google.com>
2020-10-04 21:43:13 -0700

    Add firebase auth to demo app

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const mustacheExpress = require('mustache-express');
 
 // Constants
 const PORT = 8080;
@@ -8,9 +9,21 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+
+// Register '.mustache' extension with The Mustache Express
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views')
+
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-  res.send('Hello');
+    res.render('index', {
+        "name": "world!"
+    })
 });
+
+
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
